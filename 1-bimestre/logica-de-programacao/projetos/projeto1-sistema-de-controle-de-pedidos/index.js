00722b282bc1fd91c83d950c20393c37c85b3c16
Lucas Almeida <lucasalvesrealms@gmail.com>
2026-08-28 13:34:09 +0000

    projeto finalizado

--- 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
+++ 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
@@ -1,5 +1,42 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
+console.log
+const cliente = "Mariana Silva"
+const cidade="Fortaleza"
+const produto="Notebook Pro"
+const categoria="Notebook"
+const preco = (3500)
+const quantidade =(2)
+const descontoPercentual =(10)
+const valorPago = 7000
+const subtotal =7000
+const desconto = 10
+const valorDesconto = 700
+const valorFinal= 6300
+const troco = 700
+const resumo=(`==================================================
+TECHSTORE SISTEMAS
+RESUMO DO PEDIDO
+==================================================
 
+Cliente: ${cliente}
+Cidade: ${cidade}
+
+Produto: ${produto}
+Categoria: ${categoria}
+Quantidade: ${quantidade}
+Preço unitário: ${preco}
+
+Subtotal: R$ ${subtotal}
+Desconto: ${descontoPercentual}
+Valor do desconto: ${valorPago}
+Valor final: R$ ${valorFinal}
+
+Valor pago: R$ ${valorPago}
+Troco: R$ ${troco}
+
+Obrigado pela compra!
+==================================================`)
+console.log(resumo)
 
 
 
